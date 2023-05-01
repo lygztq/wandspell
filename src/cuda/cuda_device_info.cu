05c195ae903d7ff37e53e8b86e60eaa880f33db3
lygztq <lygztq@gmail.com>
2023-05-01 20:56:25 +0800

    better

--- src/cuda/cuda_device_info.cu
+++ src/cuda/cuda_device_info.cu
@@ -7,9 +7,13 @@ void DumpDeviceInfo(int device_id) {
   CUDART_CALL(cudaGetDeviceProperties(&prop, device_id));
   std::cout << "===\nDevice " << device_id << ": " << std::endl;
   std::cout << "Device Name: " << prop.name << std::endl;
-  std::cout << "Num of SM: " << prop.multiProcessorCount << std::endl;
+  std::cout << "Arch: sm" << prop.major << prop.minor << std::endl;
+  std::cout << "Global Memory: " << prop.totalGlobalMem / 1024 / 1024 / 1024 << " GB" << std::endl;
   std::cout << "Clock Rate: " << prop.clockRate / 1000 << " Mhz" << std::endl;
+  std::cout << "Num of SM: " << prop.multiProcessorCount << std::endl;
+  std::cout << "Cuda Core Per SM: " << _ConvertSMVer2Cores(prop.major, prop.minor) << std::endl;
   std::cout << "Max Blocks Per SM: " << prop.maxBlocksPerMultiProcessor << std::endl;
+  std::cout << "Max Threads Per SM: " << prop.maxThreadsPerMultiProcessor << std::endl;
   std::cout << "Shared Memory Per Block: " << prop.sharedMemPerBlock / 1024 << " KB" << std::endl;
 }
 
